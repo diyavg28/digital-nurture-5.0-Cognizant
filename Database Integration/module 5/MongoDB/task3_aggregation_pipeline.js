99a5f23201af5ae9f45ef1a831b9170ff214c7bf
Diya V <diyavg28@gmail.com>
2026-06-20 14:22:39 +0530

    Module 5 Task 3 Aggregation Pipeline

---
+++ Database Integration/module 5/MongoDB/task3_aggregation_pipeline.js
@@ -0,0 +1,74 @@
+use("college_nosql")
+
+db.feedback.aggregate([
+  {
+    $match: {
+      semester: "2022-ODD"
+    }
+  },
+  {
+    $group: {
+      _id: "$course_code",
+      avg_rating: { $avg: "$rating" },
+      total_feedback: { $sum: 1 }
+    }
+  },
+  {
+    $sort: {
+      avg_rating: -1
+    }
+  }
+])
+
+db.feedback.aggregate([
+  {
+    $match: {
+      semester: "2022-ODD"
+    }
+  },
+  {
+    $group: {
+      _id: "$course_code",
+      avg_rating: { $avg: "$rating" },
+      total_feedback: { $sum: 1 }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      course_code: "$_id",
+      average_rating: { $round: ["$avg_rating", 1] },
+      total_feedback: 1
+    }
+  },
+  {
+    $sort: {
+      average_rating: -1
+    }
+  }
+])
+
+db.feedback.aggregate([
+  {
+    $unwind: "$tags"
+  },
+  {
+    $group: {
+      _id: "$tags",
+      count: { $sum: 1 }
+    }
+  },
+  {
+    $sort: {
+      count: -1
+    }
+  }
+])
+
+db.feedback.createIndex({
+  course_code: 1
+})
+
+db.feedback.find({
+  course_code: "CS101"
+}).explain("executionStats")
