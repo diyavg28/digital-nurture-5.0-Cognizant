885161d7541dffd1c4a2e949ec65371b859dfda6
Diya V <diyavg28@gmail.com>
2026-07-02 22:12:36 +0530

    Completed module 7 task 2

--- FrontendDevelopment/student-portal-angular/src/app/course-card/course-card.ts
+++ FrontendDevelopment/student-portal-angular/src/app/course-card/course-card.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './course-card.html',
   styleUrl: './course-card.scss'
 })
 export class CourseCard {
 
   @Input() name!: string;
-  @Input() code!: string;
-  @Input() credits!: number;
+  @Input() code!: any;
+  @Input() credits!: any;
   @Input() grade!: string;
 
 }
--- FrontendDevelopment/student-portal-angular/src/app/course-list/course-list.ts
+++ FrontendDevelopment/student-portal-angular/src/app/course-list/course-list.ts
@@ -1,28 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CourseCard } from '../course-card/course-card';
+import { CourseService } from '../services/course';
 
 @Component({
   selector: 'app-course-list',
+  standalone: true,
   imports: [CommonModule, FormsModule, CourseCard],
   templateUrl: './course-list.html',
   styleUrl: './course-list.scss',
 })
-export class CourseList {
+export class CourseList implements OnInit {
 
-  courses = [
-    { name: 'Data Structures', code: 'CS101', credits: 4, grade: 'A' },
-    { name: 'Operating Systems', code: 'CS102', credits: 3, grade: 'B+' },
-    { name: 'Database Management', code: 'CS103', credits: 4, grade: 'A' },
-    { name: 'Computer Networks', code: 'CS104', credits: 3, grade: 'A+' },
-    { name: 'Web Development', code: 'CS105', credits: 2, grade: 'A' }
-  ];
+  courses: any[] = [];
+  loading = false;
   searchTerm = '';
-  get filteredCourses() {
-  return this.courses.filter(course =>
-    course.name.toLowerCase().includes(this.searchTerm.toLowerCase())
-  );
+
+  constructor(private courseService: CourseService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+  console.log("ngOnInit called");
+
+  this.loading = true;
+
+  this.courseService.getCourses().subscribe({
+    next: (data) => {
+      console.log("SUCCESS", data);
+      this.courses = data;
+      
+      this.loading = false;
+      this.cdr.detectChanges();
+      
+      
+    },
+    error: (err) => {
+      console.log("ERROR", err);
+      this.loading = false;
+      console.log(this.loading);
+      console.log(this.courses.length);
+    }
+  });
 }
 
+  get filteredCourses() {
+    return this.courses.filter(course =>
+      course.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
