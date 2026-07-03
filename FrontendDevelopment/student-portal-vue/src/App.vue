3846282d330f80a2b4e55e71393ba1e09f29e75b
Diya V <diyavg28@gmail.com>
2026-07-03 13:06:27 +0530

    completed Module 8 - Task 2

--- FrontendDevelopment/student-portal-vue/src/App.vue
+++ FrontendDevelopment/student-portal-vue/src/App.vue
@@ -1,7 +1,30 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <CoursesView />
+  <nav>
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/courses">Courses</RouterLink> |
+    <RouterLink to="/profile">Profile</RouterLink>
+  </nav>
+
+  <hr>
+
+  <RouterView />
 </template>
 
-<script setup lang="ts">
-import CoursesView from './views/CoursesView.vue'
-</script>
+<style scoped>
+nav {
+  padding: 15px;
+}
+
+nav a {
+  margin-right: 15px;
+  text-decoration: none;
+  color: blue;
+}
+
+nav a.router-link-active {
+  font-weight: bold;
+}
+</style>
---
+++ FrontendDevelopment/student-portal-vue/src/views/CourseDetailView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+const courses = [
+  { id: 1, name: 'Mathematics', code: 'MAT101', credits: 4, grade: 'A' },
+  { id: 2, name: 'Physics', code: 'PHY102', credits: 3, grade: 'B+' },
+  { id: 3, name: 'Chemistry', code: 'CH103', credits: 3, grade: 'A+' },
+  { id: 4, name: 'English', code: 'ENG104', credits: 2, grade: 'A' },
+  { id: 5, name: 'Programming', code: 'CS105', credits: 4, grade: 'O' }
+]
+
+const course = courses.find(
+  c => c.id === Number(route.params.id)
+)
+function enroll(){
+  router.push('/profile')
+}
+</script>
+
+<template>
+  <div v-if="course">
+    <h2>Course Details</h2>
+
+    <p><b>Name:</b> {{ course.name }}</p>
+    <p><b>Code:</b> {{ course.code }}</p>
+    <p><b>Credits:</b> {{ course.credits }}</p>
+    <p><b>Grade:</b> {{ course.grade }}</p>
+    <button @click="enroll">Enroll</button>
+  </div>
+
+  <div v-else>
+    <h2>Course Not Found</h2>
+  </div>
+</template>
