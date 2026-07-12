a0755b4a76bdb443d667a024fd580af188d60d34
Diya V <diyavg28@gmail.com>
2026-07-12 12:58:14 +0530

    Completed MOdule 3

--- BackendFrameworks/Diya/handson_01/coursemanager/courses/urls.py
+++ BackendFrameworks/Diya/handson_01/coursemanager/courses/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from .views import hello_view
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    CourseViewSet,
+    StudentViewSet,
+    EnrollmentViewSet
+)
+
+router = DefaultRouter()
+
+router.register('courses', CourseViewSet)
+router.register('students', StudentViewSet)
+router.register('enrollments', EnrollmentViewSet)
 
 urlpatterns = [
-    path("hello/", hello_view),
+    path('', include(router.urls)),
 ]
--- BackendFrameworks/Diya/handson_01/coursemanager/courses/views.py
+++ BackendFrameworks/Diya/handson_01/coursemanager/courses/views.py
@@ -1,4 +1,37 @@
-from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-def hello_view(request):
-    return HttpResponse("Course Management API is running")
+from .models import Course, Student, Enrollment
+from .serializers import (
+    CourseSerializer,
+    StudentSerializer,
+    EnrollmentSerializer
+)
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(detail=True, methods=['get'])
+    def students(self, request, pk=None):
+        course = self.get_object()
+
+        enrollments = Enrollment.objects.filter(course=course)
+
+        students = [e.student for e in enrollments]
+
+        serializer = StudentSerializer(students, many=True)
+
+        return Response(serializer.data)
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
